4d62f19eed4835e3f6d4de3b6b42a2eda2f34b0f
untitledDev9 <ahmedsuleiman940@gmail.com>
2025-08-20 22:58:02 +0100

    cleared the slate for words

---
+++ utils/words/igboWords.js
@@ -0,0 +1,26 @@
+import water from "../../assets/glass-of-water.png";
+
+const VOCAB = [
+  { id: 1, word: "Mmiri", pronunciation: "mee-ree", meaning: "Water", image: water, eg: "Achọ m mmiri.", english: "I want water." },
+  { id: 2, word: "Ụlọ", pronunciation: "oo-loh", meaning: "House", image: water, eg: "Aga m n’ụlọ.", english: "I am going home." },
+  { id: 3, word: "Nri", pronunciation: "n-ree", meaning: "Food", image: water, eg: "Achọ m nri.", english: "I want food." },
+  { id: 4, word: "Akwụkwọ", pronunciation: "ah-kwoo-kwo", meaning: "Book", image: water, eg: "A na m agụ akwụkwọ.", english: "I am reading a book." },
+  { id: 5, word: "Anụmanụ", pronunciation: "ah-noo-mah-noo", meaning: "Animal", image: water, eg: "Anụmanụ nọ n’ọhịa.", english: "An animal is in the forest." },
+  { id: 6, word: "Ugbo ala", pronunciation: "oo-gboh ah-lah", meaning: "Car", image: water, eg: "Azụrụ m ugbo ala ọhụrụ.", english: "I bought a new car." },
+  { id: 7, word: "Nne", pronunciation: "neh-neh", meaning: "Mother", image: water, eg: "Nne m dị ezigbo mma.", english: "My mother is very kind." },
+  { id: 8, word: "Nna", pronunciation: "nah-nah", meaning: "Father", image: water, eg: "Nna m na-arụ ọrụ.", english: "My father is working." },
+  { id: 9, word: "Enyi", pronunciation: "eh-nyee", meaning: "Friend", image: water, eg: "Enyi m dị ebe a.", english: "My friend is here." },
+  { id: 10, word: "Ugbo elu", pronunciation: "oo-gboh eh-loo", meaning: "Airplane", image: water, eg: "Ugbo elu abịala.", english: "The airplane has arrived." },
+  { id: 11, word: "Ụbọchị", pronunciation: "oo-bohn-chee", meaning: "Day", image: water, eg: "Ụbọchị taa mara mma.", english: "Today is a good day." },
+  { id: 12, word: "Eluigwe", pronunciation: "ay-loo-ee-gweh", meaning: "Sky/Heaven", image: water, eg: "Eluigwe mara mma taa.", english: "The sky is beautiful today." },
+  { id: 13, word: "Osikapa", pronunciation: "oh-see-kah-pah", meaning: "Rice", image: water, eg: "Achọ m iri osikapa.", english: "I want to eat rice." },
+  { id: 14, word: "Akpụkpọ ụkwụ", pronunciation: "ah-kpoo-kpo uh-kwoo", meaning: "Shoes", image: water, eg: "Yirila m akpụkpọ ụkwụ ọhụrụ.", english: "I wore new shoes." },
+  { id: 15, word: "Akwà", pronunciation: "ah-kwah", meaning: "Cloth", image: water, eg: "Akwà m mara mma.", english: "My cloth is beautiful." },
+  { id: 16, word: "Ego", pronunciation: "eh-goh", meaning: "Money", image: water, eg: "Enwere m ego buru ibu.", english: "I have a lot of money." },
+  { id: 17, word: "Ije", pronunciation: "ee-jeh", meaning: "Journey/Walk", image: water, eg: "Anyị gara ije.", english: "We went for a walk." },
+  { id: 18, word: "Azụ", pronunciation: "ah-zoo", meaning: "Fish", image: water, eg: "Azụrụ m azụ.", english: "I bought fish." },
+  { id: 19, word: "Okuko", pronunciation: "oh-koo-koh", meaning: "Chicken", image: water, eg: "Okuko nọ n’ubi.", english: "The chicken is in the yard." },
+  { id: 20, word: "Obodo", pronunciation: "oh-boh-doh", meaning: "Town/City", image: water, eg: "Ebi m n’obodo ukwu.", english: "I live in a big city." },
+];
+
+export default VOCAB;
---
+++ utils/words/yorubaWords.js
@@ -0,0 +1,28 @@
+  import water from "../../assets/glass-of-water.png";
+  
+  
+const VOCAB = [
+  { id: 1, word: "Ọmi", pronunciation: "oh-mee", meaning: "Water", image: water, eg: "Mo fẹ omi.", english: "I want water." },
+  { id: 2, word: "Ilé", pronunciation: "ee-leh", meaning: "House", image: water, eg: "Mo nlọ si ile.", english: "I am going home." },
+  { id: 3, word: "Ọúnjẹ", pronunciation: "oh-oon-jeh", meaning: "Food", image: water, eg: "Mo fẹ ounjẹ.", english: "I want food." },
+  { id: 4, word: "Ìwé", pronunciation: "ee-weh", meaning: "Book", image: water, eg: "Mo ka iwe.", english: "I read a book." },
+  { id: 5, word: "Ẹranko", pronunciation: "eh-ranh-koh", meaning: "Animal", image: water, eg: "Eranko wa ni igbo.", english: "An animal is in the forest." },
+  { id: 6, word: "Ọkọ", pronunciation: "oh-kaw", meaning: "Car", image: water, eg: "Mo ra ọkọ tuntun.", english: "I bought a new car." },
+  { id: 7, word: "Ìyá", pronunciation: "ee-yah", meaning: "Mother", image: water, eg: "Ìyá mi dun gan.", english: "My mother is very kind." },
+  { id: 8, word: "Bàbá", pronunciation: "bah-bah", meaning: "Father", image: water, eg: "Baba mi n ṣiṣẹ.", english: "My father is working." },
+  { id: 9, word: "Ọrẹ", pronunciation: "oh-reh", meaning: "Friend", image: water, eg: "Ọrẹ mi wa nibi.", english: "My friend is here." },
+  { id: 10, word: "Ọkọ̀ òfurufú", pronunciation: "oh-kaw ofu-ru-fu", meaning: "Airplane", image: water, eg: "Ọkọ̀ òfurufú ti de.", english: "The airplane has arrived." },
+  { id: 11, word: "Ọjọ́", pronunciation: "oh-jo", meaning: "Day", image: water, eg: "Ọjọ́ loni dara.", english: "Today is a good day." },
+  { id: 12, word: "Ọrun", pronunciation: "oh-roon", meaning: "Sky/Heaven", image: water, eg: "Ọrun dára loni.", english: "The sky is beautiful today." },
+  { id: 13, word: "Ìrẹsì", pronunciation: "ee-reh-si", meaning: "Rice", image: water, eg: "Mo fẹ jẹ iresi.", english: "I want to eat rice." },
+  { id: 14, word: "Bàtà", pronunciation: "bah-tah", meaning: "Shoes", image: water, eg: "Mo wọ bata tuntun.", english: "I wore new shoes." },
+  { id: 15, word: "Aṣọ", pronunciation: "ah-shaw", meaning: "Cloth", image: water, eg: "Aso mi lẹwa.", english: "My cloth is beautiful." },
+  { id: 16, word: "Owó", pronunciation: "oh-woh", meaning: "Money", image: water, eg: "Mo ní owo púpọ̀.", english: "I have a lot of money." },
+  { id: 17, word: "Ìrìn", pronunciation: "ee-reen", meaning: "Journey/Walk", image: water, eg: "A lọ irin.", english: "We went for a walk." },
+  { id: 18, word: "Ẹja", pronunciation: "eh-jah", meaning: "Fish", image: water, eg: "Mo ra ẹja.", english: "I bought fish." },
+  { id: 19, word: "Adìẹ", pronunciation: "ah-dee-eh", meaning: "Chicken", image: water, eg: "Adie wa ninu agbala.", english: "The chicken is in the yard." },
+  { id: 20, word: "Ìlú", pronunciation: "ee-loo", meaning: "Town/City", image: water, eg: "Mo n gbe ni ilu nla.", english: "I live in a big city." },
+];
+
+
+  export default VOCAB
