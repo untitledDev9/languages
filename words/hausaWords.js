4d4c515ba7b285a71236c21e048fe6651282374a
KwinZainy <gbadamosiblessing651@gmail.com>
2025-08-22 23:47:29 +0100

    igbo and hausa

---
+++ words/hausaWords.js
@@ -0,0 +1,47 @@
+import water from "../images/hausa/water.jpg";
+import house from '../images/hausa/house.jpg'
+import food from '../images/hausa/food.jpg'
+import book from '../images/hausa/book.jpg'
+import animals from '../images/hausa/animals.jpg'
+import car from '../images/hausa/car.jpg'
+import mother from '../images/hausa/mother.jpg'
+import father from '../images/hausa/father.jpg'
+import friend from '../images/hausa/friend.jpg'
+import airplane from '../images/hausa/airplane.jpg'
+import day from '../images/hausa/day.jpg'
+import sky from '../images/hausa/sky.jpg'
+import rice from '../images/hausa/rice.jpg'
+import shoes from '../images/hausa/shoes.jpg'
+import clothes from '../images/hausa/clothes.jpg'
+import money from '../images/hausa/money.jpg'
+import walk from '../images/hausa/walk.jpg'
+import fish from '../images/hausa/fish.jpg'
+import chicken from '../images/hausa/chicken.jpg'
+import city from '../images/hausa/city.jpg'
+
+
+
+const VOCAB = [
+  { id: 1, word: "Ruwa", pronunciation: "roo-wah", meaning: "Water", image: water, eg: "Ina so ruwa.", english: "I want water." },
+  { id: 2, word: "Gida", pronunciation: "gee-dah", meaning: "House", image: house, eg: "Zan tafi gida.", english: "I am going home." },
+  { id: 3, word: "Abinci", pronunciation: "ah-been-chee", meaning: "Food", image: food, eg: "Ina son abinci.", english: "I want food." },
+  { id: 4, word: "Littafi", pronunciation: "lee-tah-fee", meaning: "Book", image: book, eg: "Ina karanta littafi.", english: "I am reading a book." },
+  { id: 5, word: "Dabbobi", pronunciation: "dah-boh-bee", meaning: "Animals", image: animals, eg: "Dabbobi suna cikin daji.", english: "Animals are in the forest." },
+  { id: 6, word: "Mota", pronunciation: "moh-tah", meaning: "Car", image: car, eg: "Na sayi sabuwar mota.", english: "I bought a new car." },
+  { id: 7, word: "Uwa", pronunciation: "oo-wah", meaning: "Mother", image: mother, eg: "Uwata tana da kirki.", english: "My mother is kind." },
+  { id: 8, word: "Uba", pronunciation: "oo-bah", meaning: "Father", image: father, eg: "Ubana yana aiki.", english: "My father is working." },
+  { id: 9, word: "Aboki", pronunciation: "ah-boh-kee", meaning: "Friend", image: friend, eg: "Abokina yana nan.", english: "My friend is here." },
+  { id: 10, word: "Jirgin sama", pronunciation: "jeer-geen sah-mah", meaning: "Airplane", image: airplane, eg: "Jirgin sama ya iso.", english: "The airplane has arrived." },
+  { id: 11, word: "Rana", pronunciation: "rah-nah", meaning: "Day", image: day, eg: "Rana ta yi kyau yau.", english: "Today is a good day." },
+  { id: 12, word: "Sama", pronunciation: "sah-mah", meaning: "Sky/Heaven", image: sky, eg: "Sama ta yi kyau.", english: "The sky is beautiful." },
+  { id: 13, word: "Shinkafa", pronunciation: "sheen-kah-fah", meaning: "Rice", image: rice, eg: "Ina so in ci shinkafa.", english: "I want to eat rice." },
+  { id: 14, word: "Takalma", pronunciation: "tah-kahl-mah", meaning: "Shoes", image: shoes, eg: "Na sa sababbin takalma.", english: "I wore new shoes." },
+  { id: 15, word: "Tufafi", pronunciation: "too-fah-fee", meaning: "Clothes", image: clothes, eg: "Tufafina sun yi kyau.", english: "My clothes are beautiful." },
+  { id: 16, word: "Kudi", pronunciation: "koo-dee", meaning: "Money", image: money, eg: "Ina da kudi da yawa.", english: "I have a lot of money." },
+  { id: 17, word: "Tafiya", pronunciation: "tah-fee-yah", meaning: "Journey/Walk", image: walk, eg: "Mun yi tafiya.", english: "We went on a walk." },
+  { id: 18, word: "Kifi", pronunciation: "kee-fee", meaning: "Fish", image: fish, eg: "Na saya kifi.", english: "I bought fish." },
+  { id: 19, word: "Kaji", pronunciation: "kah-jee", meaning: "Chicken", image: chicken, eg: "Kaji suna cikin gida.", english: "The chickens are in the yard." },
+  { id: 20, word: "Gari", pronunciation: "gah-ree", meaning: "Town/City", image: city, eg: "Ina zaune a cikin gari.", english: "I live in a city." },
+];
+
+export default VOCAB;
---
+++ words/igboWords.js
@@ -0,0 +1,45 @@
+import water from "../images/igbo/wateri.jpg";
+import house from '../images/igbo/housei.jpg'
+import food from '../images/igbo/foodii.jpg'
+import book from '../images/igbo/booki.jpg'
+import animal from '../images/igbo/animalsi.jpg'
+import car from '../images/igbo/cari.jpg'
+import mother from '../images/igbo/motheri.jpg'
+import father from '../images/igbo/fatheri.jpg'
+import friend from '../images/igbo/friendi.jpg'
+import airplane from '../images/igbo/airplanei.jpg'
+import day from '../images/igbo/dayi.jpg'
+import sky from '../images/igbo/skyi.jpg'
+import rice from '../images/igbo/ricei.jpg'
+import shoe from '../images/igbo/shoei.jpg'
+import cloth from '../images/igbo/cloth.jpg'
+import money from '../images/igbo/moneyi.jpg'
+import walk from '../images/igbo/walki.jpg'
+import fish from '../images/igbo/fishi.jpg'
+import chicken from '../images/igbo/chickeni.jpg'
+import city from '../images/igbo/cityi.jpg'
+
+const VOCAB = [
+  { id: 1, word: "Mmiri", pronunciation: "mee-ree", meaning: "Water", image: water, eg: "Achọ m mmiri.", english: "I want water." },
+  { id: 2, word: "Ụlọ", pronunciation: "oo-loh", meaning: "House", image: house, eg: "Aga m n’ụlọ.", english: "I am going home." },
+  { id: 3, word: "Nri", pronunciation: "n-ree", meaning: "Food", image: food, eg: "Achọ m nri.", english: "I want food." },
+  { id: 4, word: "Akwụkwọ", pronunciation: "ah-kwoo-kwo", meaning: "Book", image: book, eg: "A na m agụ akwụkwọ.", english: "I am reading a book." },
+  { id: 5, word: "Anụmanụ", pronunciation: "ah-noo-mah-noo", meaning: "Animal", image: animal, eg: "Anụmanụ nọ n’ọhịa.", english: "An animal is in the forest." },
+  { id: 6, word: "Ugbo ala", pronunciation: "oo-gboh ah-lah", meaning: "Car", image: car, eg: "Azụrụ m ugbo ala ọhụrụ.", english: "I bought a new car." },
+  { id: 7, word: "Nne", pronunciation: "neh-neh", meaning: "Mother", image: mother, eg: "Nne m dị ezigbo mma.", english: "My mother is very kind." },
+  { id: 8, word: "Nna", pronunciation: "nah-nah", meaning: "Father", image: father, eg: "Nna m na-arụ ọrụ.", english: "My father is working." },
+  { id: 9, word: "Enyi", pronunciation: "eh-nyee", meaning: "Friend", image: friend, eg: "Enyi m dị ebe a.", english: "My friend is here." },
+  { id: 10, word: "Ugbo elu", pronunciation: "oo-gboh eh-loo", meaning: "Airplane", image: airplane, eg: "Ugbo elu abịala.", english: "The airplane has arrived." },
+  { id: 11, word: "Ụbọchị", pronunciation: "oo-bohn-chee", meaning: "Day", image: day, eg: "Ụbọchị taa mara mma.", english: "Today is a good day." },
+  { id: 12, word: "Eluigwe", pronunciation: "ay-loo-ee-gweh", meaning: "Sky/Heaven", image: sky, eg: "Eluigwe mara mma taa.", english: "The sky is beautiful today." },
+  { id: 13, word: "Osikapa", pronunciation: "oh-see-kah-pah", meaning: "Rice", image: rice, eg: "Achọ m iri osikapa.", english: "I want to eat rice." },
+  { id: 14, word: "Akpụkpọ ụkwụ", pronunciation: "ah-kpoo-kpo uh-kwoo", meaning: "Shoes", image: shoe, eg: "Yirila m akpụkpọ ụkwụ ọhụrụ.", english: "I wore new shoes." },
+  { id: 15, word: "Akwà", pronunciation: "ah-kwah", meaning: "Cloth", image: cloth, eg: "Akwà m mara mma.", english: "My cloth is beautiful." },
+  { id: 16, word: "Ego", pronunciation: "eh-goh", meaning: "Money", image: money, eg: "Enwere m ego buru ibu.", english: "I have a lot of money." },
+  { id: 17, word: "Ije", pronunciation: "ee-jeh", meaning: "Journey/Walk", image: walk, eg: "Anyị gara ije.", english: "We went for a walk." },
+  { id: 18, word: "Azụ", pronunciation: "ah-zoo", meaning: "Fish", image: fish, eg: "Azụrụ m azụ.", english: "I bought fish." },
+  { id: 19, word: "Okuko", pronunciation: "oh-koo-koh", meaning: "Chicken", image: chicken, eg: "Okuko nọ n’ubi.", english: "The chicken is in the yard." },
+  { id: 20, word: "Obodo", pronunciation: "oh-boh-doh", meaning: "Town/City", image: city, eg: "Ebi m n’obodo ukwu.", english: "I live in a big city." },
+];
+
+export default VOCAB;
